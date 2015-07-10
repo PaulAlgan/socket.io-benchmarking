16d77faee207c1b5add4f235c87f9adbc83ada40
phatthana <phatthana@me.com>
2015-07-10 17:45:06 +0700

    update express

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-var app = require('express').createServer(),
+var express = require('express'),
     io = require('socket.io').listen(app),
     logger = require('winston'),
     program = require('commander');
-    
-//  
+
+var app = express();
+//
 // SETUP
 //
 
 logger.cli();
 logger.default.transports.console.timestamp = true;
-    
+
 program.version('0.1')
     .option('-p, --port [num]', 'Set the server port (default 8080)')
     .option('-H, --disableheartbeats', 'Disable heartbeats')
     .parse(process.argv)
-    
+
 var server = "localhost";
 if(program.args.length==1) {
     server = program.args[0];
@@ -54,11 +55,11 @@ var messagesPerSecond = 0;
 
 io.sockets.on('connection', function(socket) {
     connectedUsersCount++;
-    
+
     socket.on('chat', function(data) {
         // logger.info("chat message arrived");
         io.sockets.emit('chat', {text:data.text});
-        
+
         messagesPerSecond++;
     });
 
@@ -75,9 +76,8 @@ setTimeout(logStatus, 1000);
 
 function logStatus() {
     setTimeout(logStatus, 1000);
-    
-    
+
+
     logger.info("users: " + connectedUsersCount + "\tmessagesPerSecond: " + messagesPerSecond);
     messagesPerSecond = 0;
 }
-
